87a11cd3473162327639ebf747ab8efc2582c842
William Bartholomew <wdb@willbar.com>
2016-10-28 15:54:57 -0700

    Move crawler into its own file

--- index.js
+++ index.js
@@ -22,92 +22,10 @@ Graph Navigation:
 * Links are unordered
 */
 
-const request = require('request');
-const moment = require('moment');
-const seen = {};
 const crawlqueue = require('./lib/crawlqueue');
 const queue = new crawlqueue();
+const crawler = require('./lib/crawler');
 queue.push({ type: 'orgs', url: 'https://api.github.com/user/orgs' });
 
-processNext();
-
-function processNext() {
-  const crawlRequest = queue.pop();
-  if (!crawlRequest) {
-    return;
-  }
-  if (!seen[crawlRequest.url]) {
-    request.get(crawlRequest.url + '?per_page=100', { headers: { 'User-Agent': 'ghcrawler' }, json: true }, function (err, response, body) {
-      seen[crawlRequest.url] = true;
-      body._metadata = {
-        type: crawlRequest.type,
-        url: crawlRequest.url,
-        fetchedAt: moment.utc().toISOString(),
-        links: []
-      };
-      switch (crawlRequest.type) {
-        case 'orgs': {
-          processCollection(body, 'login', crawlRequest.context);
-          break;
-        }
-        case 'repo': {
-          processRepo(body, crawlRequest.context);
-          break;
-        }
-        case 'login': {
-          processLogin(body, crawlRequest.context);
-          break;
-        }
-        case 'repos': {
-          processCollection(body, 'repo', crawlRequest.context);
-          break;
-        }
-        case 'issues': {
-          processCollection(body, 'issue', crawlRequest.context);
-          break;
-        }
-        case 'issue': {
-          processIssue(body, crawlRequest.context);
-          break;
-        }
-      }
-      console.dir(body);
-      setTimeout(processNext, 0);
-    }).auth('', process.env['GITHUB_TOKEN']);
-  }
-  else {
-    setTimeout(processNext, 0);
-  }
-}
-
-function processCollection(document, type, context) {
-  document.forEach(item => {
-    queue.push({ type: type, url: item.url, context: context });
-  });
-  return null;
-}
-
-function processRepo(document) {
-  document._metadata.links.push({ 'self': { type: 'self', 'href': `urn:repo:${document.id}` } });
-  document._metadata.links.push({ 'owner': { type: 'self', href: `urn:login:${document.owner.id}` } });
-  document._metadata.links.push({ 'parent': { type: 'self', href: `urn:login:${document.owner.id}` } });
-  document._metadata.links.push({ 'siblings': { type: 'siblings', href: `urn:login:${document.owner.id}:repos` } });
-  queue.push({ type: 'login', url: document.owner.url });
-  queue.push({ type: 'issues', url: document.issues_url.replace('{/number}', ''), context: {repo: document } });
-  return document;
-}
-
-function processLogin(document) {
-  document._metadata.links.push({ 'self': { type: 'self', href: `urn:login:${document.id}` } });
-  document._metadata.links.push({ 'repos': { type: 'siblings', href: `urn:login:${document.id}:repos` } });
-  document._metadata.links.push({ 'siblings': { type: 'siblings', href: 'urn:login' } });
-  queue.push({ type: 'repos', url: document.repos_url });
-  return document;
-}
-
-function processIssue(document, context) {
-  document._metadata.links.push({ 'self': { type: 'self', href: `urn:repo:${context.repo.id}:issue:${document.id}` } });
-  document._metadata.links.push({ 'siblings': { type: 'siblings', href: `urn:repo:${context.repo.id}:issues` } });
-  document._metadata.links.push({ 'assignees': { type: 'self', hrefs: document.assignees.map(assignee => { return `urn:login:${assignee.id}`}) } });
-  return document;
-}
+const engine = new crawler(queue);
+engine.start();
