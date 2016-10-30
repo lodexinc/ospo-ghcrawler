597ad911c9e1b0dc9a634b70e84b17ceded7d5cc
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-10-30 14:44:18 -0700

    simplify test response structure

--- test/githubRequestTests.js
+++ test/githubRequestTests.js
@@ -1,6 +1,5 @@
 const assert = require('chai').assert;
 const chai = require('chai');
-const config = require('painless-config');
 const expect = require('chai').expect;
 const extend = require('extend');
 const GitHubRequest = require('../lib/githubRequest.js');
@@ -20,6 +19,7 @@ describe('Request option merging', () => {
     expect(result.options.retryDelay).to.equal(10);
     expect(result.options.testProperty).to.equal('test value');
   });
+
   it('should merge and override headers', () => {
     const result = new GitHubRequest({
       headers: {
@@ -34,15 +34,21 @@ describe('Request option merging', () => {
 
 describe('Request retry and success', () => {
   it('should be able to get a single page resource', () => {
-    const request = createRequest();
+    const responses = [createSingleResponse({ id: 'cool object' })];
+    const request = createRequest(responses);
     request.get(`${urlHost}/singlePageResource`).then(result => {
       expect(result.id).to.equal('cool object');
       const activity = request.activity[0];
       expect(activity.attempts).to.equal(1);
     });
   });
+
   it('should be able to get a multi page resource', () => {
-    const request = createRequest();
+    const responses = [
+      createMultiPageResponse('twoPageResource', [{ page: 1 }], null, 2),
+      createMultiPageResponse('twoPageResource', [{ page: 2 }], 1, null)
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/twoPageResource`).then(result => {
       expect(result.length).to.equal(2);
       expect(result[0].page).to.equal(1);
@@ -53,8 +59,16 @@ describe('Request retry and success', () => {
       assert.fail();
     });
   });
+
   it('should retry 500 errors and eventually fail', () => {
-    const request = createRequest();
+    const responses = [
+      createSingleResponse('bummer', 500),
+      createSingleResponse('bummer', 500),
+      createSingleResponse('bummer', 500),
+      createSingleResponse('bummer', 500),
+      createSingleResponse('bummer', 500)
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/serverError`).then(result => {
       // TODO what should be the right return value from a 500?
       // The body of the response or the response itself?
@@ -66,8 +80,14 @@ describe('Request retry and success', () => {
       assert.fail();
     });
   });
+
   it('should retry 500 errors and eventually succeed', () => {
-    const request = createRequest();
+    const responses = [
+      createSingleResponse('bummer', 500),
+      createSingleResponse({ id: 1 }),
+      createSingleResponse({ id: 2 })
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/retry500succeed`).then(result => {
       expect(result.id).to.equal(1);
       const activity = request.activity[0];
@@ -77,8 +97,16 @@ describe('Request retry and success', () => {
       assert.fail();
     });
   });
+
   it('should retry network errors and eventually fail', () => {
-    const request = createRequest();
+    const responses = [
+      createErrorResponse('bummer'),
+      createErrorResponse('bummer'),
+      createErrorResponse('bummer'),
+      createErrorResponse('bummer'),
+      createErrorResponse('bummer')
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/networkError`).then(result => {
       assert.fail();
     }, err => {
@@ -88,8 +116,15 @@ describe('Request retry and success', () => {
       expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
     });
   });
+
   it('should retry network errors and eventually succeed', () => {
-    const request = createRequest();
+    const responses = [
+      createErrorResponse('bummer 1'),
+      createErrorResponse('bummer 2'),
+      createSingleResponse({ id: 1 }),
+      createSingleResponse({ id: 2 })
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/retryNetworkErrorSucceed`).then(result => {
       expect(result.id).to.equal(1);
       const activity = request.activity[0];
@@ -101,20 +136,14 @@ describe('Request retry and success', () => {
       assert.fail();
     });
   });
-  it('should recover after network errors', () => {
-    const request = createRequest();
-    return request.getAll(`${urlHost}/retryNetworkErrorSucceed`).then(result => {
-      expect(result.id).to.equal(1);
-      const activity = request.activity[0];
-      expect(activity.attempts).to.equal(3);
-      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
-      expect(activity.delays[1].retry).to.equal(request.options.retryDelay);
-    }, err => {
-      assert.fail();
-    });
-  });
+
   it('should recover after 403 forbidden', () => {
-    const request = createRequest();
+    const responses = [
+      createSingleResponse('forbidden 1', 403),
+      createSingleResponse({ id: 1 }),
+      createSingleResponse({ id: 2 })
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/forbidden`).then(result => {
       expect(result.id).to.equal(1);
       const activity = request.activity[0];
@@ -125,8 +154,15 @@ describe('Request retry and success', () => {
       assert.fail();
     });
   });
+
   it('should recover after error and deliver all pages', () => {
-    const request = createRequest();
+    const responses = [
+      // createMultiPageResponse(target, body, previous, next, last, code = 200, error = null, remaining = 4000) {
+      createMultiPageResponse('pagedWithErrors', [{ page: 1 }], null, 2),
+      createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null, 2, null, 'bummer'),
+      createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null)
+    ];
+    const request = createRequest(responses);
     return request.getAll(`${urlHost}/pagedWithErrors`).then(result => {
       expect(result.length).to.equal(2);
       expect(result[0].page).to.equal(1);
@@ -147,93 +183,25 @@ describe('Request retry and success', () => {
   });
 });
 
-function createRequest() {
+function createRequest(responses) {
   // initialize the hook each time to ensure a fresh copy of the response table
-  initializeRequestHook();
+  initializeRequestHook(responses);
   return new GitHubRequest({
-    headers: {
-      authorization: `token ${config.get("GITHUB_CRAWLER_TOKEN")}`
-    },
     retryDelay: 10,
     forbiddenDelay: 15
   });
 }
 
-function createResponseTable() {
-  const responseTable = {
-    singlePageResource: { type: 'single', response: createSingleResponse({ id: 'cool object' }) },
-    twoPageResource: {
-      type: 'paged',
-      responses: [
-        createMultiPageResponse('twoPageResource', [{ page: 1 }], null, 2),
-        createMultiPageResponse('twoPageResource', [{ page: 2 }], 1, null)
-      ]
-    },
-    serverError: { type: 'single', response: createSingleResponse('bummer', 500) },
-    networkError: { type: 'single', response: createErrorResponse('bummer') },
-    retry500succeed: {
-      type: 'sequenced',
-      responses: [
-        createSingleResponse('bummer', 500),
-        createSingleResponse({ id: 1 }),
-        createSingleResponse({ id: 2 })
-      ]
-    },
-    retryNetworkErrorSucceed: {
-      type: 'sequenced',
-      responses: [
-        createErrorResponse('bummer 1'),
-        createErrorResponse('bummer 2'),
-        createSingleResponse({ id: 1 }),
-        createSingleResponse({ id: 2 })
-      ]
-    },
-    forbidden: {
-      type: 'sequenced',
-      responses: [
-        createSingleResponse('forbidden 1', 403),
-        createSingleResponse({ id: 1 }),
-        createSingleResponse({ id: 2 })
-      ]
-    },
-    pagedWithErrors: {
-      type: 'sequenced',
-      responses: [
-        // createMultiPageResponse(target, body, previous, next, last, code = 200, error = null, remaining = 4000) {
-        createMultiPageResponse('pagedWithErrors', [{ page: 1 }], null, 2),
-        createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null, 2, null, 'bummer'),
-        createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null)
-      ]
-    }
-  };
-  return extend(true, {}, responseTable);
-}
-
-// hook the node request object to bypass the actuall network sending and do the thing we want.
-function initializeRequestHook() {
-  const responses = createResponseTable();
+// hook the node request object to bypass the actual network sending and do the thing we want.
+function initializeRequestHook(responseList) {
+  const responses = responseList.slice();
   const hook = (options, callback) => {
-    const target = options.url;
-    const data = responses[url.parse(target).pathname.replace(/^\//, "")];
-    let result = null;
-    switch (data.type) {
-      case 'single':
-        result = data.response;
-        break;
-      case 'paged':
-        query = querystring.parse(url.parse(target).query);
-        page = query.page ? query.page : 1;
-        result = data.responses[page - 1];
-        break;
-      case 'sequenced':
-        result = data.responses.shift();
-    }
-
     // finish the call in a timeout to simulate the network call context switch
-    callback(result.error, result.response, result.response.body);
-    // setTimeout(() => {
-    //   callback(result.error, result.response, result.response.body);
-    // }, 0);
+    // callback(result.error, result.response, result.response.body);
+    setTimeout(() => {
+      result = responses.shift();
+      callback(result.error, result.response, result.response.body);
+    }, 0);
   };
   request.Request.request = hook;
 }
