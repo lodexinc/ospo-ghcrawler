47e26ff1a23dca1675989b28156345d7170e6ce5
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-10-30 14:14:50 -0700

    rename fetcher to request, add more tests

--- test/githubRequestTests.js
+++ test/githubRequestTests.js
@@ -1,8 +1,9 @@
+const assert = require('chai').assert;
 const chai = require('chai');
 const config = require('painless-config');
-const expect = chai.expect;
+const expect = require('chai').expect;
 const extend = require('extend');
-const GitHubFetcher = require('../lib/githubFetcher.js');
+const GitHubRequest = require('../lib/githubRequest.js');
 const Q = require('q');
 const querystring = require('querystring');
 const request = require('requestretry');
@@ -10,104 +11,146 @@ const url = require('url');
 
 const urlHost = 'https://test.com';
 
-describe('Basic fetcher success', () => {
+describe('Request option merging', () => {
+  it('should merge and override properties', () => {
+    const result = new GitHubRequest({
+      retryDelay: 10,
+      testProperty: 'test value'
+    });
+    expect(result.options.retryDelay).to.equal(10);
+    expect(result.options.testProperty).to.equal('test value');
+  });
+  it('should merge and override headers', () => {
+    const result = new GitHubRequest({
+      headers: {
+        'User-Agent': 'test agent',
+        authorization: 'test auth'
+      }
+    });
+    expect(result.options.headers['User-Agent']).to.equal('test agent');
+    expect(result.options.headers['authorization']).to.equal('test auth');
+  });
+});
+
+describe('Request retry and success', () => {
   it('should be able to get a single page resource', () => {
-    const fetcher = getFetcher();
-    fetcher.get(`${urlHost}/singlePageResource`).then(result => {
+    const request = createRequest();
+    request.get(`${urlHost}/singlePageResource`).then(result => {
       expect(result.id).to.equal('cool object');
-      const activity = fetcher.activity[0];
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(1);
     });
   });
   it('should be able to get a multi page resource', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/twoPageResource`).then(result => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/twoPageResource`).then(result => {
       expect(result.length).to.equal(2);
       expect(result[0].page).to.equal(1);
       expect(result[1].page).to.equal(2);
-      expect(fetcher.activity[0].attempts).to.equal(1);
-      expect(fetcher.activity[1].attempts).to.equal(1);
+      expect(request.activity[0].attempts).to.equal(1);
+      expect(request.activity[1].attempts).to.equal(1);
     }, err => {
-      fail();
+      assert.fail();
     });
   });
   it('should retry 500 errors and eventually fail', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/serverError`).then(result => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/serverError`).then(result => {
       // TODO what should be the right return value from a 500?
       // The body of the response or the response itself?
       expect(result).to.equal('bummer');
-      const activity = fetcher.activity[0];
-      expect(activity.attempts).to.equal(fetcher.options.maxAttempts);
-      expect(activity.delays[0].retry).to.equal(fetcher.options.retryDelay);
+      const activity = request.activity[0];
+      expect(activity.attempts).to.equal(request.options.maxAttempts);
+      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
     }, err => {
-      fail();
+      assert.fail();
     });
   });
   it('should retry 500 errors and eventually succeed', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/retry500succeed`).then(result => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/retry500succeed`).then(result => {
       expect(result.id).to.equal(1);
-      const activity = fetcher.activity[0];
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(2);
-      expect(activity.delays[0].retry).to.equal(fetcher.options.retryDelay);
+      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
     }, err => {
-      fail();
+      assert.fail();
     });
   });
   it('should retry network errors and eventually fail', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/networkError`).then(result => {
-      fail();
+    const request = createRequest();
+    return request.getAll(`${urlHost}/networkError`).then(result => {
+      assert.fail();
     }, err => {
       expect(err).to.equal('bummer');
-      const activity = fetcher.activity[0];
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(5);
-      expect(activity.delays[0].retry).to.equal(fetcher.options.retryDelay);
+      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
     });
   });
   it('should retry network errors and eventually succeed', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/retryNetworkErrorSucceed`).then(result => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/retryNetworkErrorSucceed`).then(result => {
       expect(result.id).to.equal(1);
-      const activity = fetcher.activity[0];
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(3);
       expect(activity.delays.length).to.equal(2);
-      expect(activity.delays[0].retry).to.equal(fetcher.options.retryDelay);
-      expect(activity.delays[1].retry).to.equal(fetcher.options.retryDelay);
+      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
+      expect(activity.delays[1].retry).to.equal(request.options.retryDelay);
     }, err => {
-      fail();
+      assert.fail();
     });
   });
-  it('should recover after network errors', (done) => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/retryNetworkErrorSucceed`, [], (err, body) => {
-      expect(err).is.equal(null);
-      expect(body.id).to.equal(1);
-      const activity = fetcher.activity[0];
+  it('should recover after network errors', () => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/retryNetworkErrorSucceed`).then(result => {
+      expect(result.id).to.equal(1);
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(3);
-      expect(activity.delays[0].retry).to.equal(fetcher.options.retryDelay);
-      expect(activity.delays[1].retry).to.equal(fetcher.options.retryDelay);
-      done();
+      expect(activity.delays[0].retry).to.equal(request.options.retryDelay);
+      expect(activity.delays[1].retry).to.equal(request.options.retryDelay);
+    }, err => {
+      assert.fail();
     });
   });
   it('should recover after 403 forbidden', () => {
-    const fetcher = getFetcher();
-    return fetcher.getAll(`${urlHost}/forbidden`).then(result => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/forbidden`).then(result => {
       expect(result.id).to.equal(1);
-      const activity = fetcher.activity[0];
+      const activity = request.activity[0];
       expect(activity.attempts).to.equal(2);
       expect(activity.delays.length).to.equal(1);
-      expect(activity.delays[0].forbidden).to.equal(fetcher.options.forbiddenDelay);
+      expect(activity.delays[0].forbidden).to.equal(request.options.forbiddenDelay);
     }, err => {
-      fail();
+      assert.fail();
+    });
+  });
+  it('should recover after error and deliver all pages', () => {
+    const request = createRequest();
+    return request.getAll(`${urlHost}/pagedWithErrors`).then(result => {
+      expect(result.length).to.equal(2);
+      expect(result[0].page).to.equal(1);
+      expect(result[1].page).to.equal(2);
+
+      expect(request.activity.length).to.equal(2);
+      const activity0 = request.activity[0];
+      expect(activity0.attempts).to.equal(1);
+      expect(activity0.delays).to.be.undefined;
+
+      const activity1 = request.activity[1];
+      expect(activity1.attempts).to.equal(2);
+      expect(activity1.delays.length).to.equal(1);
+      expect(activity1.delays[0].retry).to.equal(request.options.retryDelay);
+    }, err => {
+      assert.fail();
     });
   });
 });
 
-function getFetcher() {
+function createRequest() {
+  // initialize the hook each time to ensure a fresh copy of the response table
   initializeRequestHook();
-  return new GitHubFetcher({
+  return new GitHubRequest({
     headers: {
       authorization: `token ${config.get("GITHUB_CRAWLER_TOKEN")}`
     },
@@ -152,6 +195,15 @@ function createResponseTable() {
         createSingleResponse({ id: 1 }),
         createSingleResponse({ id: 2 })
       ]
+    },
+    pagedWithErrors: {
+      type: 'sequenced',
+      responses: [
+        // createMultiPageResponse(target, body, previous, next, last, code = 200, error = null, remaining = 4000) {
+        createMultiPageResponse('pagedWithErrors', [{ page: 1 }], null, 2),
+        createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null, 2, null, 'bummer'),
+        createMultiPageResponse('pagedWithErrors', [{ page: 2 }], 1, null)
+      ]
     }
   };
   return extend(true, {}, responseTable);
@@ -186,27 +238,28 @@ function initializeRequestHook() {
   request.Request.request = hook;
 }
 
-function createSingleResponse(body, code = 200) {
+function createSingleResponse(body, code = 200, remaining = 4000) {
   return {
     // error: null,
     response: {
       status: code,
       headers: {
-        'x-ratelimit-remaining': 4000
+        'x-ratelimit-remaining': remaining
       },
       body: body
     }
   }
 }
 
-function createMultiPageResponse(target, body, previous, next, last) {
+function createMultiPageResponse(target, body, previous, next, last, code = 200, error = null, remaining = 4000) {
   return {
-    // error: null,
+    error: error,
     response: {
       headers: {
-        'x-ratelimit-remaining': 4000,
+        'x-ratelimit-remaining': remaining,
         link: createLinkHeader(target, previous, next, last)
       },
+      status: code,
       body: body
     }
   };
