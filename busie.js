db441a8c405731df40919654636d2aa3f3c26eec
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-11-18 09:45:12 -0800

    add exit to busie

--- busie.js
+++ busie.js
@@ -4,4 +4,4 @@ const logger = OspoCrawler.createLogger(false, true);
 const queues = OspoCrawler.createServiceBusQueues(logger);
 const seedRequests = [OspoCrawler.createSeedRequest('orgs', 'https://api.github.com/user/orgs', 'urn:microsoft/orgs')];
 
-queues.push(seedRequests);
+queues.push(seedRequests).finally(() => process.exit());
