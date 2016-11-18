6a64a61e17e0e1f2b5e475727859bf22151199cc
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-11-18 15:34:00 -0800

    improve configurability

--- busie.js
+++ busie.js
@@ -1,7 +1,9 @@
 const OspoCrawler = require('./lib/ospoCrawler');
 
+const topic = process.argv[2];
+const queueProvider = process.argv[3] || 'amqp';
 const logger = OspoCrawler.createLogger(false, true);
-const queues = OspoCrawler.createServiceBusQueues(logger);
+const queues = OspoCrawler.createQueues(queueProvider, topic, logger);
 const seedRequests = [OspoCrawler.createSeedRequest('orgs', 'https://api.github.com/user/orgs', 'urn:microsoft/orgs')];
 
 queues.push(seedRequests).finally(() => process.exit());
