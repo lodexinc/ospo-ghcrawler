5f105ae6b97a17e35a46f88056d0a91a5ca80c3e
Maggie Pint <magpint@microsoft.com>
2016-12-07 10:49:57 -0800

    starts in memory

--- runLocal.js
+++ runLocal.js
@@ -1,12 +1,12 @@
 const OspoCrawler = require('./lib/ospoCrawler');
 const Crawler = require('ghcrawler').crawler;
 
-const logger = OspoCrawler.createLogger(false, true);
-const queues = OspoCrawler.createAmqpQueues(logger);
-const store = OspoCrawler.createStore();
-const locker = OspoCrawler.createLocker();
-const requestorInstance = OspoCrawler.createRequestor();
+const logger = OspoCrawler.createLogger(true, true);
 const options = OspoCrawler.createOptions();
+const queues = OspoCrawler.createAmqpQueues(options.queuing);
+const store = OspoCrawler.createStore(options.storage);
+const locker = OspoCrawler.createLocker(options.locker);
+const requestorInstance = OspoCrawler.createRequestor();
 // options.promiseTrace = true;
 const crawler = new Crawler(queues, store, locker, requestorInstance, options, logger);
 const seedRequests = [OspoCrawler.createSeedRequest('orgs', 'https://api.github.com/user/orgs', 'urn:microsoft/orgs')];
