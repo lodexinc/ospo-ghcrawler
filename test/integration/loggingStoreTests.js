c005cfcc9265442d96a0cfc2bcf303b66968b7ed
Gene Hazan <ghazan@microsoft.com>
2016-12-09 11:57:51 -0800

    Handle parallelism and BlockCountExceedsLimit error. Add unit & integration tests.

--- test/integration/loggingStoreTests.js
+++ test/integration/loggingStoreTests.js
@@ -1,5 +1,6 @@
 const OspoCrawler = require('../../lib/ospoCrawler');
 const Q = require('q');
+const qlimit = require('qlimit');
 
 let loggingStore;
 
@@ -28,7 +29,22 @@ describe('Logging Store Integration', () => {
     return loggingStore.connect().then(() => {
       return loggingStore.upsert({ test: process.hrtime().join('') }).then(() => {
         return loggingStore.upsert({ test: process.hrtime().join('') });
-      })
+      });
+    });
+  });
+
+  it('Should connect and upsert many times', () => {
+    const document = { abc: 1 };
+    const docs = [];
+    for (let i=0; i<50; i++) {
+      docs.push(document);
+    }
+    let counter = 0;
+    return loggingStore.connect().then(() => {
+      return Q.all(docs.map(qlimit(10)(doc => {
+        console.log(++counter);
+        return loggingStore.upsert(doc);
+      })));
     });
   });
 });
